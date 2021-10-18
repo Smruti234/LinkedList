6d71493fb01ebd9cfee2f133ceeced854593e17e
Smruti <lenkasmruti123@gmail.com>
2021-10-18 23:11:23 +0530

    Added: Uc3_LinkedListAppend

--- JavaPractice/MyLinkedList.java
+++ JavaPractice/MyLinkedList.java
@@ -20,6 +20,23 @@ public class MyLinkedList {
 						 myLinkedList.tail.equals(myFirstNode);
 		Assert.assertTrue(result);	
     }
+
+
+@Test
+public void given3NumbersWhenAppendedShouldBeAddedToList() {		
+	MyNode<Integer>myFirstNode  = new MyNode<Integer>(56);
+	MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+	MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+	LinkedListTest myLinkedList = new LinkedListTest();
+	myLinkedList.add(myFirstNode);
+	myLinkedList.append(mySecondNode);
+	myLinkedList.append(myThirdNode);
+	myLinkedList.printMyNodes();
+	boolean result = myLinkedList.head.equals(myFirstNode)&&
+					 myLinkedList.head.getNext().equals(mySecondNode)&&
+					 myLinkedList.tail.equals(myThirdNode);
+	Assert.assertTrue(result);	
+}
 }
 
 
